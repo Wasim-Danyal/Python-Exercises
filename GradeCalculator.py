279fae339a06a19460c7ec652a2af3123a0a0afd
Wasim-Danyal <68053858+Wasim-Danyal@users.noreply.github.com>
2020-07-17 18:09:57 +0100

    Add files via upload

---
+++ GradeCalculator.py
@@ -0,0 +1,24 @@
+def FinalGrades(per): 
+    if per>= 70:
+        return 'Congratulations! You got an A'
+    elif per>= 60:
+        return 'You got a B'
+    elif per>= 50:
+        return 'You got a C'
+    elif per>= 40:
+        return 'You got a D'
+    else:
+        return 'Unfortunately you failed and will need to resit the exam'
+
+
+def get_grades():
+  maths = int(input('Math grade: '))
+  chemistry = int(input('Chemistry grade: '))
+  physics = int(input('Physics grade: '))
+
+  Ask = maths + chemistry + physics
+  return Ask/3
+
+percent = get_grades()
+grade = FinalGrades(percent)
+print (grade))
